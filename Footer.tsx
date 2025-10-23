37727fda46768ecb831b4860631ccf5670617e70
Carla Hoodle <carla.myhoodle@gmail.com>
2025-10-23 14:30:31 -0400

    Rename footer component to Footer

--- Footer.tsx
+++ Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const footer: React.FC = () => {
+export const Footer: React.FC = () => {
   return (
-    <footer className="text-center py-6 px-4">
+    <Footer className="text-center py-6 px-4">
       <p className="text-sm text-slate-500">
         &copy; {new Date().getFullYear()} Hoodle Content Optimizer. All rights reserved.
       </p>
