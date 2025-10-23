7575dada453d7fbafc05342c0e44a09554135b19
Carla Hoodle <carla.myhoodle@gmail.com>
2025-10-23 14:31:23 -0400

    Rename export from 'header' to 'Header'

--- Header.tsx
+++ Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const header: React.FC = () => {
+export const Header: React.FC = () => {
   return (
-    <header className="bg-white shadow-md">
+    <Header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-5 md:px-8 flex items-center justify-between">
         <div className="flex items-center space-x-3">
             <div className="p-2 bg-indigo-600 rounded-lg flex items-center justify-center h-10 w-10">
