4a435e85f0b2e7978500111ba5a5d7df85ee8086
Carla Hoodle <carla.myhoodle@gmail.com>
2025-10-23 09:02:13 -0400

    Rename Footer component to footer

--- Footer.tsx
+++ Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Footer: React.FC = () => {
+export const footer: React.FC = () => {
   return (
     <footer className="text-center py-6 px-4">
       <p className="text-sm text-slate-500">
